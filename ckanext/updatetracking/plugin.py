8c70e5615b2798e88e251524748e6dd25b6aedb8
Gutts-n <57202549+Gutts-n@users.noreply.github.com>
2025-05-15 22:22:09 -0300

    [sm] Removed not used functions

--- ckanext/updatetracking/plugin.py
+++ ckanext/updatetracking/plugin.py
@@ -3,15 +3,7 @@ import ckan.plugins.toolkit as toolkit
 from ckanext.updatetracking.commands import update_tracking
 
 class UpdatetrackingPlugin(plugins.SingletonPlugin):
-    plugins.implements(plugins.IConfigurer)
     plugins.implements(plugins.IClick)
 
-    # IConfigurer
-
-    def update_config(self, config_):
-        toolkit.add_template_directory(config_, 'templates')
-        toolkit.add_public_directory(config_, 'public')
-        toolkit.add_resource('fanstatic', 'updatetracking')
-    
     def get_commands(self):
         return [update_tracking]
